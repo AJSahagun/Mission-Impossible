667b7166ab2e96d4c336537e32b411754e71e0f3
Cloud Rain <88638495+Cl0udRain@users.noreply.github.com>
2021-12-10 23:34:30 +0800

    Update ClassnCursestest.cpp

--- ClassnCursestest.cpp
+++ ClassnCursestest.cpp
@@ -4,45 +4,111 @@
 #include <windows.h>
 using namespace std;
 
-class menu
-{
-	private:
-		// Variables for the Main Window
-		int yMax, xMax, yMid, xMid;
-
-		
-	public:
-		// Variables for the Sub Window
-		int syMax, sxMax, syMid, sxMid;
-		// Function that gets the sizes of a window
-		void get_size (WINDOW * win)
-		{
-			getmaxyx(win, syMax, sxMax);
-			syMid = syMax/2;
-			sxMid = sxMax/2;
-		}
-
-};
-
 int main ()
 {
 	// Maximize Window
-	HWND consoleWindow = GetConsoleWindow();
-    ShowWindow(consoleWindow, SW_MAXIMIZE);
+	HWND consoleWindow = GetConsoleWindow(); // This gets the value Windows uses to identify your output window 
+    ShowWindow(consoleWindow, SW_MAXIMIZE); // this mimics clicking on its' maximize button
 
 	// Initializes Curses
 	initscr();
-	noecho();
-	curs_set(0);
-	cbreak();
+	noecho(); // No keyboard echo
+	curs_set(0); // No cursor
+	cbreak(); // crtl+C exit
 	
-	menu obj;
-	obj.get_size(stdscr);
-	mvprintw(3, 9,"%d %d", obj.sxMax, obj.sxMid);
+	// Init Var 
+	int yMax, xMax;
+	getmaxyx(stdscr, yMax, xMax); // get max stdscr size
+	
+	/* MAIN WINDOW */
+	WINDOW * wMain = newwin(yMax-2, xMax-4, 1, 2); // initializes Main Win
+	box(wMain, 0, 0); // border
+	keypad(wMain, true); // keyboard input on
+
+	// Get the Mid Values of the window
+	int wyMid = (yMax-3)/3;
+	int wxMid = (xMax-6)/2;
+
+	// LOGO
+	mvwprintw(wMain, wyMid-3, wxMid-30," ______              ______   ______        _____  _________");
+	mvwprintw(wMain, wyMid-2, wxMid-30,"|_   _ `.          .' ___  |.' ____ \\      |_   _||  _   _  |");
+	mvwprintw(wMain, wyMid-1, wxMid-30,"  | | `. \\ ______ / .'   \\_|| (___ \\_|______ | |  |_/ | | \\_|");
+	mvwprintw(wMain, wyMid, wxMid-30,"  | |  | ||______|| |        _.____`.|______|| |      | |");
+	mvwprintw(wMain, wyMid+1, wxMid-30," _| |_.' /        \\ `.___.'\\| \\____) |      _| |_    _| |_");
+	mvwprintw(wMain, wyMid+2, wxMid-30,"|______.'          `.____ .' \\______.'     |_____|  |_____|");
+	mvwprintw(wMain, wyMid+3, wxMid-12,"Dictionary for CS and IT");
+	
+	refresh ();
+	wrefresh(wMain);
+
+	// Initializes Vars
+	string choices [] = {"Start", "About"};
+	int elem = sizeof(choices)/sizeof(*choices); // Get Num of elements
+	int choice, highlight = 0;
+	int elem1 = choices[0].size()/2; // Gets the mid size of a particular element
+	
+	/* MENU WINDOW */
+	WINDOW * wMenu = subwin(wMain, 8, 40, wyMid*2, wxMid-20); // init Menu Win
+	box(wMenu, 0, 0); // border
+	
+	// get sub win max size
+	int syMax, sxMax;
+	getmaxyx(wMenu, syMax, sxMax);
+	
+	// get mid of sub win
+	int swyMid = syMax/2;
+	int swxMid = sxMax/2;
+	
+	// choice printer
+	while (choice != 10) { // exits if enter key pressed (ascii enter val = 10)
+		for (int i = 0; i < elem; i++) {
+			if (i == highlight) {
+				wattron(wMenu, A_REVERSE);
+			}
+			mvwprintw(wMenu, 1+i, swxMid-(choices[i].size()/2), choices[i].c_str());
+			wattroff(wMenu, A_REVERSE);
+		}
+		touchwin(wMenu);
+		choice = wgetch(wMenu); // keyboard input
+	
+	// choice highlighter
+		switch(choice) {
+			case KEY_UP:
+				highlight--;
+				if(highlight < 0) {
+					highlight = 0;
+				}
+				break;
+			case KEY_DOWN:
+				highlight++;
+				if (highlight > 1) {
+					highlight = 1;
+				}
+				break;
+			default:
+				break;
+		}
+	}
+	
+	// converts string into array element num
+	auto itr = find(choices, choices + elem, choices[highlight]);
+	
+	// choice branch
+	switch (distance(choices, itr)) {
+		case 0:
+			mvwprintw(wMain, 3, 9,"%d", elem1);
+			wgetch(wMain);
+			break;
+		case 1:
+			break;
+	}
+	
+	//mvwprintw(wMain, 3, 9,"%d", elem1);
+	//wgetch(wMain);
 	
 	// Ends PD Curses
 	getch();
 	endwin();
-	
+
 	return 0;
 }
